601b36850ad57bdfdfec6e60ccd0cdf634991ba9
andriy <andbvko@gmail.com>
2025-05-16 22:33:26 -0400

    add forecast component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import  { ForecastComponent } from './forecast/forecast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ForecastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
